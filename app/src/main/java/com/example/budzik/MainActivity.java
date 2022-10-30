5847e693ddb472e9dff39603c365e791dc132b1c
Kamila Szydełko <kam.szydelko11@gmail.com.com>
2022-10-30 15:08:48 +0100

    Test

---
+++ app/src/main/java/com/example/budzik/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.budzik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.*;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton button_settings = findViewById(R.id.button_settings);
+        button_settings.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v){
+               System.out.println("A");
+           }
+        });
+
+        FloatingActionButton button_add = findViewById(R.id.button_add);
+        button_add.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, MenuAlarm.class));
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/com/example/budzik/MenuAlarm.java
@@ -0,0 +1,21 @@
+package com.example.budzik;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MenuAlarm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+
+
+
+    }
+}
