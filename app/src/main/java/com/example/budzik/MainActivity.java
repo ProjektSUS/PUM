46223b5a562781a52108be529b1929d6388ddd4a
Kamila Szydełko <kam.szydelko11@gmail.com.com>
2022-11-01 00:01:30 +0100

    Alarm budzik - problem z SDK 31

--- app/src/main/java/com/example/budzik/MainActivity.java
+++ app/src/main/java/com/example/budzik/MainActivity.java
@@ -1,21 +1,26 @@
 package com.example.budzik;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.*;
+import android.widget.AbsListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class MainActivity extends AppCompatActivity{
 
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Przycisk aby przejść do ustawień
         FloatingActionButton button_settings = findViewById(R.id.button_settings);
         button_settings.setOnClickListener(new View.OnClickListener(){
            @Override
@@ -24,6 +29,7 @@ public class MainActivity extends AppCompatActivity{
            }
         });
 
+        //Przycisk aby przejść do dodawania budzików
         FloatingActionButton button_add = findViewById(R.id.button_add);
         button_add.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -31,7 +37,11 @@ public class MainActivity extends AppCompatActivity{
                 startActivity(new Intent(MainActivity.this, MenuAlarm.class));
             }
         });
+
+        //Lista, która wyświetla aktualne budziki
+        recyclerView = findViewById(R.id.recyclerView);
+
+
+
     }
-
-
 }
--- app/src/main/java/com/example/budzik/MenuAlarm.java
+++ app/src/main/java/com/example/budzik/MenuAlarm.java
@@ -1,23 +1,43 @@
 package com.example.budzik;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.Switch;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class MenuAlarm extends AppCompatActivity {
 
+    Button button_addAl;
+    TimePicker timePicker;
+    int hour, minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+
+        //Ustawianie wartości zegara [TimePicker]
+        timePicker = findViewById(R.id.timePicker);
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int hour, int minute) {
+                MenuAlarm.this.hour = hour;
+                MenuAlarm.this.minute = minute;
+            }
+        });
 
         //Przycisk powrotu
         FloatingActionButton button_back_al = findViewById(R.id.button_back_al);
@@ -28,31 +48,70 @@ public class MenuAlarm extends AppCompatActivity {
             }
         });
 
-        //Przycisk dodania alarmu
-        Button button_addAl = findViewById(R.id.button_addAl);
+        //Dodawanie alarmu
+        button_addAl = findViewById(R.id.button_addAl);
         button_addAl.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                CheckBox checkBox_pon = findViewById(R.id.checkBox_pon);
-                CheckBox checkBox_wt = findViewById(R.id.checkBox_wt);
-                CheckBox checkBox_sr = findViewById(R.id.checkBox_sr);
-                CheckBox checkBox_czw = findViewById(R.id.checkBox_czw);
-                CheckBox checkBox_pt = findViewById(R.id.checkBox_pt);
-                CheckBox checkBox_sb = findViewById(R.id.checkBox_sb);
-                CheckBox checkBox_nd = findViewById(R.id.checkBox_nd);
+                //Sprawdzenie, które dni tygodnia są zaznaczone
+//                CheckBox checkBox_pn = findViewById(R.id.checkBox_pn);
+//                CheckBox checkBox_wt = findViewById(R.id.checkBox_wt);
+//                CheckBox checkBox_sr = findViewById(R.id.checkBox_sr);
+//                CheckBox checkBox_czw = findViewById(R.id.checkBox_czw);
+//                CheckBox checkBox_pt = findViewById(R.id.checkBox_pt);
+//                CheckBox checkBox_sb = findViewById(R.id.checkBox_sb);
+//                CheckBox checkBox_nd = findViewById(R.id.checkBox_nd);
+//
+//                boolean isCheck_pn = checkBox_pn.isChecked();
+//                boolean isCheck_wt = checkBox_wt.isChecked();
+//                boolean isCheck_sr = checkBox_sr.isChecked();
+//                boolean isCheck_czw = checkBox_czw.isChecked();
+//                boolean isCheck_pt = checkBox_pt.isChecked();
+//                boolean isCheck_sb = checkBox_sb.isChecked();
+//                boolean isCheck_nd = checkBox_nd.isChecked();
 
-                boolean isCheck_pn = checkBox_pon.isChecked();
-                boolean isCheck_wt = checkBox_wt.isChecked();
-                boolean isCheck_sr = checkBox_sr.isChecked();
-                boolean isCheck_czw = checkBox_czw.isChecked();
-                boolean isCheck_pt = checkBox_pt.isChecked();
-                boolean isCheck_sb = checkBox_sb.isChecked();
-                boolean isCheck_nd = checkBox_nd.isChecked();
-
+                System.out.println("A");
+                setTimer(v);
 
 
             }
         });
 
     }
+
+    //Ustawienie alarmu
+    public void setTimer(View v){
+        AlarmManager alarmManager = (AlarmManager)getSystemService((Context.ALARM_SERVICE));
+        Date date = new Date();
+        Calendar cal_alarm = Calendar.getInstance();
+        Calendar cal_now = Calendar.getInstance();
+        cal_now.setTime(date);
+        cal_alarm.setTime(date);
+
+        cal_alarm.set(Calendar.HOUR_OF_DAY, hour);
+        cal_alarm.set(Calendar.MINUTE, minute);
+        cal_alarm.set(Calendar.SECOND,0);
+
+        if(cal_alarm.before(cal_now)){
+            cal_alarm.add(Calendar.DATE,1);
+        }
+
+
+        Intent intent = new Intent(MenuAlarm.this,MyBroadcastReceiver.class);
+        PendingIntent pendingIntent;
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
+            pendingIntent = PendingIntent.getActivity(MenuAlarm.this, 123, intent, PendingIntent.FLAG_IMMUTABLE);
+        } else{
+            pendingIntent = PendingIntent.getActivity(MenuAlarm.this, 123, intent,PendingIntent.FLAG_ONE_SHOT);
+        }
+        alarmManager.set(AlarmManager.RTC_WAKEUP,cal_alarm.getTimeInMillis(),pendingIntent);
+
+    }
+
 }
+
+//Brak błędu przy przyciskaniu "dodaj alarm", ale nie działa xDDD
+//Tryb ciemny byłby fajen
+//Sprawdzić te FLAG na nowo, co to dokładnie robi i jak działa z getActivities
+//Dni tygodnia, custom wiadomość i custom title dodać
+//
